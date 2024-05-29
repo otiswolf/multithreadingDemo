80780cfa21ec81e2778c5edc4a73b23cef2b25ef
Otis Wolf <wolfotis@gmail.com>
2024-05-29 16:34:39 -0500

    Implement locks using synchronization blocks

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,21 +5,37 @@ public class Main {
 
         System.out.println("main() is starting");
 
-        Thread thread1 = new Thread1("myThread1");
+//        Thread thread1 = new Thread1("myThread1");
+//
+////        thread1.setDaemon(true);
+//        thread1.start();
+//
+////        Thread thread2 = new Thread(new Thread2(), "myThread2");
+//
+//        // An alternate way of implementing runnables
+//        Thread thread2 = new Thread(() -> {
+//            for (int i = 0; i < 5; i++) {
+//                System.out.println("inside " + Thread.currentThread() + ": " + i);
+//            }
+//        }, "myThread2");
+//
+//        thread2.start();
 
-//        thread1.setDaemon(true);
-        thread1.start();
+        Stack stack = new Stack(5);
 
-//        Thread thread2 = new Thread(new Thread2(), "myThread2");
-
-        // An alternate way of implementing runnables
-        Thread thread2 = new Thread(() -> {
-            for (int i = 0; i < 5; i++) {
-                System.out.println("inside " + Thread.currentThread() + ": " + i);
+        new Thread(() -> {
+            int counter = 0;
+            while(++ counter < 10) {
+                System.out.println("Pushed: " + stack.push(100));
             }
-        }, "myThread2");
+        }, "Pusher").start();
 
-        thread2.start();
+        new Thread(() -> {
+            int counter = 0;
+            while(++ counter < 10) {
+                System.out.println("Popped: " + stack.pop());
+            }
+        }, "Popper").start();
 
         System.out.println("main() is exiting");
     }
